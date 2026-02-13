74b8b270212b09357729d1b6f1da95ec3b86222e
cjxymm <cjade756@gmail.com>
2026-02-12 20:23:17 -0500

    Initial commit: React static page

---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+function Content() {
+  return (
+    <main>
+      <p>
+        The hare mocked the slow tortoise and challenged him to a race.
+      </p>
+      <p>
+        Confident he would win, the hare stopped to rest while the tortoise kept moving.
+      </p>
+      <p>
+        The tortoise passed the sleeping hare and won the race.
+      </p>
+    </main>
+  );
+}
+
+export default Content;
